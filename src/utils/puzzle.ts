174a10107eb78c91bcedd11d64311ba856f583f5
Akash Shah <akash.w.shah@gmail.com>
2024-05-03 19:30:50 -0400

    Improve puzzle type

--- src/utils/puzzle.ts
+++ src/utils/puzzle.ts
@@ -1,22 +1,53 @@
+import { AvatarIdent } from '@pkmn/protocol';
 import { Team } from '@pkmn/sets';
 
 type BattleCommand = {
-    kind: 'move' | 'switch' | 'setswitch',
+    action: 'move' | 'switch' | 'setswitch',
     name: string
 };
 
 type CommandGroup = {
-    kind: 'group',
+    action: 'group',
     commands: BattleCommand[]
 };
 
 export type Command = BattleCommand | CommandGroup;
 
 export type Puzzle = {
+    /**
+     * The name of the puzzle.
+     */
     name?: string,
+    /**
+     * The author of the puzzle.
+     */
     author?: string,
+    /**
+     * The description of the puzzle.
+     */
     description?: string,
+    /**
+     * The team for the bot to use for the puzzle.
+     */
     team?: Team,
+    /**
+     * The sequence of commands to execute during the puzzle.
+     */
     commands?: Command[],
-    hints?: string[]
+    /**
+     * The hints to be given during the puzzle at the user's request.
+     */
+    hints?: string[],
+    /**
+     * Whether critical hits are allowed to occur during the puzzle. The bot should forfeit if a critical hit occurs and this is `false`.
+     */
+    crits?: boolean,
+    /**
+     * Whether the timer should be turned on during the puzzle, at the beginning of the battle.
+     */
+    timer?: boolean,
+    /**
+     * The avatar the bot should use for the puzzle.
+     */
+    avatar?: AvatarIdent
 };
