ea19ae75973f36f6a74f9262b258d260a743aa2f
Akash Shah <akash.w.shah@gmail.com>
2024-05-13 22:24:02 -0400

    LabeledElement throws error if given element does not have id

--- src/components/LabeledElement.tsx
+++ src/components/LabeledElement.tsx
@@ -4,6 +4,9 @@ import styles from './LabeledElement.module.css';
  * Creates an element with the given label and an abbreviation given by the description. The element must be given a unique id. 
  */
 function LabeledElement({ label, element, description }: { label: string, element: React.JSX.Element, description?: string }) {
+  if (!element.props.id) {
+    throw new Error(`Element ${element} does not have an id`);
+  }
   return (
     <div className={styles.labeledElement}>
       <label htmlFor={element.props.id}><abbr title={description}>{`${label}:`}</abbr> </label>
