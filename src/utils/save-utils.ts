604b192819c5ead7ef730d59c396f9ecf49e105c
Akash Shah <akash.w.shah@gmail.com>
2024-05-16 00:37:21 -0400

    Save utils print and fill partial puzzles on initial load

--- src/utils/save-utils.ts
+++ src/utils/save-utils.ts
@@ -1,13 +1,22 @@
 import { getCookie, setCookie } from "typescript-cookie";
-import { Puzzle } from "./puzzle";
+import { Puzzle, puzzleDefaults } from "./puzzle";
+import { print } from "./print-colored";
 
-function getPuzzles(): Puzzle[] {
+function getSavedPuzzles(): Puzzle[] {
+    // Get cookie
     const puzzlesCookie = getCookie('puzzles');
-    return puzzlesCookie ? JSON.parse(puzzlesCookie) : [];
+    // Parse JSON if the cookie existed before, use an empty array otherwise
+    // Then cast to array of partial puzzles
+    const partials: Partial<Puzzle>[] = puzzlesCookie ? JSON.parse(puzzlesCookie) : [];
+    // Fill in defaults for partial puzzles and cast to full puzzle
+    const puzzles = partials.map(puzzle => { return { ...puzzleDefaults, ...puzzle } as Puzzle; });
+    print(`Loaded puzzles: ${JSON.stringify(puzzles)}`);
+    return puzzles;
 }
 
 function savePuzzles(puzzles: Puzzle[]) {
     setCookie('puzzles', JSON.stringify(puzzles));
+    print(`Saved puzzles: ${JSON.stringify(puzzles)}`);
 }
 
-export { getPuzzles, savePuzzles };
+export { getSavedPuzzles, savePuzzles };
